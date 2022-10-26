fe8c68809c2668e55c84b0bc53133ed3427dddea
nickedchen <1049379870@qq.com>
2022-10-26 12:19:34 -0700

    Initial Commit

---
+++ ResCode/ResCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResCodeApp.swift
+//  ResCode
+//
+//  Created by Nick Chen on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct ResCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
